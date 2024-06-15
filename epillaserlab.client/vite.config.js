160c55f42c5eece5c168e47add0ce1ed993fa496
fsdsfd <vitalijspagin1@gmail.com>
2024-06-15 09:27:04 +0300

    asadssaddas

--- epillaserlab.client/vite.config.js
+++ epillaserlab.client/vite.config.js
@@ -52,14 +52,14 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '/api':{
-                target: 'http://localhost:5000',
-                changeOrigin: true,
-            },
-            '/resources':{
-                target: 'http://localhost:5000',
-                changeOrigin: true,
-            }
+            // '/api':{
+            //     target: 'http://localhost:5000',
+            //     changeOrigin: true,
+            // },
+            // '/resources':{
+            //     target: 'http://localhost:5000',
+            //     changeOrigin: true,
+            // }
         },
         port: 7171,
         https: {
